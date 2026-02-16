c813cc1d8df77e5cba24d0e25ab6e23f9dacde7a
Fastering18 <brahmana165@gmail.com>
2026-02-16 12:57:10 +0700

    refactor: clean cpp workspace

--- main.cpp
+++ main.cpp
@@ -6,18 +6,5 @@ using namespace std;
 //static const long long mod = 1e9 + 7;
 
 int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    string s;
-    int res = 0;
-    cin >> s;
-
-    while (res > 9) {
-      for (int i=0; i<(int)(s.size()); i++) res += (int)(s[i]);
-      s = (string)
-    }
-  }
-
   return 0;
 }
